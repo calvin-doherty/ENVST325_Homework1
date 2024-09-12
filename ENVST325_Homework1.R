8797cee127fd7571bf82c95a0d0a827551be33d9
calvin-doherty <cdoherty@hamilton.edu>
2024-09-12 12:33:42 +0000

    description of changes

---
+++ ENVST325_Homework1.R
@@ -0,0 +1,34 @@
+# In Class Question 1
+snow <- c(2.5,3,5,4.5)
+snow.cm <-2.54*(snow)
+
+# In Class Question 2
+
+highpeaks <- data.frame(
+  name=c("Haystack", "Skylight", "Dix", "Gray"),
+  elevation.ft=c(4960,4926,4857,4840), 
+  ascent.ft=c(3570,4265,2800,4178),
+  length.mi=c(17.8,17.9,13.2,16))
+
+# In Class Question 3
+
+temp.c <- c(-44,0,20,35)
+temp.f <- 9/5*(temp.c) + 32
+
+# Homework Question 1
+
+elevation.ft <- c(4960,4926,4857,4840)
+elevation.m <- 0.3048*(elevation.ft)
+
+# Homework Question 2
+
+#If you only typed code in console, none of the code would save 
+#or be able to be changed at a later date. Because the code wouldn't
+#save, it would not follow version control practices.
+
+#Homework Question 3
+
+longest.trip <- highpeaks[2,]
+  
+
+
